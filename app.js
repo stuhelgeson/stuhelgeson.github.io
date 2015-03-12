65e51192091235de0cbd4d40d99528f6e0505673
stuhelgeson <helgeson@seas.upenn.edu>
2015-03-12 17:01:43 -0400

    Version 2.3
    Added Projects and Project Slider

--- app.js
+++ app.js
@@ -44,6 +44,36 @@ var main=function(){
                 },200);
             }
         });
+//Project Slides Control
+        $('.arrow-next').click(function(){
+            var currentSlide = $('.active-slide');
+            var nextSlide = currentSlide.next();
+            var currentDot = $('.active-dot');
+            var nextDot = currentDot.next();
+            if(nextSlide.length==0){
+                nextSlide = $('.slide').first();
+                nextDot = $('.dot').first();
+            }
+            $(currentSlide).fadeOut(1).removeClass('active-slide');
+            $(nextSlide).fadeIn(1000).addClass('active-slide');
+            $(currentDot).removeClass('active-dot');
+            $(nextDot).addClass('active-dot');
+        });
+        $('.arrow-prev').click(function(){
+            var currentSlide = $('.active-slide');
+            var prevSlide = currentSlide.prev();
+            var currentDot = $('.active-dot');
+            var prevDot = currentDot.prev();
+            if(prevSlide.length==0){
+                prevSlide = $('.slide').last();
+                prevDot = $('.dot').last();
+            }
+            $(currentSlide).fadeOut(1).removeClass('active-slide');
+            $(prevSlide).fadeIn(1000).addClass('active-slide');
+            $(currentDot).removeClass('active-dot');
+            $(prevDot).addClass('active-dot');
+        });
+
 };
 
 $(document).ready(main);
